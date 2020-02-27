b52588860943f385bd7da0c0431406a9d3ea9e38
AIEDevClass <61553405+AIEDevClass@users.noreply.github.com>
2020-02-27 17:58:03 +0200

    Capitec atm code
    please don't edit shit

---
+++ Bank.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitecBank
+{
+    public class Bank
+    {
+        private int pin = 3445;
+        private double balance = 25000;
+
+        private int userPin;
+
+       //==
+       public  void setPin(int p)
+       {
+            this.userPin = p;
+       }
+
+       //==
+       public int getPin()
+       {
+            return userPin;
+       }
+
+        //==
+        public bool validatePin()
+        {
+            bool isValid = false;
+
+            if(getPin() == pin)
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+
+        //==
+        public double Deposit(double amount)
+        {
+            balance =  balance + amount;
+            return balance;
+        }
+
+        //==
+        public double Withdraw(double amount)
+        {
+            balance =  balance - amount;
+            return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitecBank
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int tries = 3;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Enter your pin");
+                    int pin = Convert.ToInt32(Console.ReadLine());
+                    Bank bank = new Bank();
+
+                    bank.setPin(pin);
+                    bool isValid = bank.validatePin();
+
+                    if(isValid)
+                    {
+                        Console.WriteLine("Specify the type of transaction you wish to perform\n\n1 = Deposit\n2 = Withdrawal");
+                        int transType = int.Parse(Console.ReadLine());
+
+                        if(transType == 1)
+                        {
+                            Console.WriteLine("Please specify the amount you wish to deposit");
+                            double amount = double.Parse(Console.ReadLine());
+                            double newBal = bank.Deposit(amount);
+
+                            Console.WriteLine("\n\nYour new balance is R" + newBal);
+                        }
+                        else if(transType == 2)
+                        {
+                            Console.WriteLine("Please specify the amount you wish to withdraw");
+                            double amount = double.Parse(Console.ReadLine());
+                            double newBal = bank.Withdraw(amount);
+
+                            Console.WriteLine("\n\nYour new balance is R" + newBal);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\nInvalid selection!!!!!!!");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: \nWrong pin!!!!!!!");
+                        tries--; 
+                    }
+                }
+                while (tries != 0);
+            }
+            catch(Exception ge)
+            {
+                Console.WriteLine("\n\nError: " + ge.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
